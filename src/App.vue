b01c4426be51185d363f489e9f9d73721c5bff32
Eduardo Martínez <eduardo.mzhz@gmail.com>
2020-04-05 16:18:18 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">HELLO WORLD...</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return { }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import App from './App.vue';
+
+Vue.use(Buefy);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
