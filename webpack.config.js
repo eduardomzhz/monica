8dd319e1e9bf087bb99543ef6e0ce4a7b7db29dd
Eduardo Martínez <eduardo.mzhz@gmail.com>
2020-04-13 22:12:48 -0500

    Fix development webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === 'development') {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"development"'
       }
     }),
     new HtmlWebpackPlugin({
