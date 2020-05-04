3b6ebe4b6a176712d5dec4f8af0e1ce9685f9ec9
Eduardo Martínez <eduardo.mzhz@gmail.com>
2020-05-03 21:44:22 -0500

    Fix frappe library build error with uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -76,10 +77,10 @@ if (process.env.NODE_ENV === 'development') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
+    new UglifyJsPlugin({
+      "uglifyOptions": {
+        sourceMap: true,
+        compress: { }
       }
     }),
     new webpack.LoaderOptionsPlugin({
